f22714ef879a969cdc3eb3bf00f7a41ff2177d99
Ignacio Paricio <ignacio.paricio@outlook.com>
2023-10-14 16:58:34 +0200

    Make telescope use eog on images

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -2,5 +2,26 @@ local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
 vim.keymap.set('n', '<leader>ps', function()
-	builtin.grep_string({ search = vim.fn.input("Grep > ") });
+    builtin.grep_string({ search = vim.fn.input("Grep > ") });
 end)
+
+_G.open_with_eog = function()
+    local entry = require("telescope.actions.state").get_selected_entry()
+    local filepath = entry and entry.value or nil
+    if filepath then
+        print("Attempting to open: " .. filepath)
+        os.execute('eog ' .. filepath .. ' &')
+    end
+end
+require('telescope').setup {
+    defaults = {
+        mappings = {
+            i = {
+                ["<CR>"] = function(prompt_bufnr)
+                    require("telescope.actions").close(prompt_bufnr)
+                    _G.open_with_eog()
+                end
+            },
+        },
+    },
+}
