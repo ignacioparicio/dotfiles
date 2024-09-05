c8a52cf44d737555de7a1b2d5c64f15df0d3395d
Ignacio Paricio <ignacio.paricio@pxl-vision.com>
2024-09-05 14:26:34 +0200

    Update TypeScript LSP name

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -23,7 +23,7 @@ require('mason').setup({})
 require('mason-lspconfig').setup({
     -- Replace the language servers listed here
     -- with the ones you want to install
-    ensure_installed = { 'tsserver', 'rust_analyzer', 'lua_ls', 'eslint', 'pylsp' },
+    ensure_installed = { 'ts_ls', 'rust_analyzer', 'lua_ls', 'eslint', 'pylsp' },
     handlers = {
         lsp_zero.default_setup,
         require('lspconfig').pylsp.setup {
