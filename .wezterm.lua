6fa416e20d6dfeeafdbd5c40502dbab8a6dc58d5
Ignacio Paricio <ignacio.paricio@pxl-vision.com>
2025-07-04 10:27:36 +0200

    Add `.wezterm.lua`

---
+++ .wezterm.lua
@@ -0,0 +1,14 @@
+local wezterm = require("wezterm")
+
+return {
+	hide_tab_bar_if_only_one_tab = true,
+
+	window_padding = {
+		left = 0,
+		right = 0,
+		top = 0,
+		bottom = 0,
+	},
+
+	window_background_opacity = 0.8,
+}
